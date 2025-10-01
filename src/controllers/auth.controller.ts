d5533c52949e792560043e21107006e35c0ab272
lakhatekno <lakhatekno2514@gmail.com>
2025-10-01 15:58:06 +0700

    [Auth] Auth Service

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,38 @@
+import { Request, Response, NextFunction } from 'express';
+import { LoginType } from '../services/auth.service';
+import { AuthServices } from '../services/auth.service';
+
+const authServices = new AuthServices();
 export class AuthController {
-  
+	static async login(req: Request, res: Response, next: NextFunction) {
+		try {
+			const { username, password }: LoginType = req.body;
+			const token = await authServices.login({ username: username, password: password });
+      res.json(token);
+		} catch (e) {
+      next(e);
+    }
+	}
+
+  static async logout(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { token } = req.body;
+      const out = await authServices.logout(token);
+      
+      res.json(out);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async refreshToken(req: Request, res: Response, next: NextFunction){
+    try {
+      const { token } = req.body;
+      const { accessToken } = await authServices.refresh(token);
+
+      res.json({ accessToken });
+    } catch (e) {
+      next(e);
+    }
+  }
 }
