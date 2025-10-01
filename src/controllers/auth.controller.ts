00cc587762af34a138f78ade2804b28e60a988e3
lakhatekno <lakhatekno2514@gmail.com>
2025-10-01 16:14:45 +0700

    [Auth] Error handling validating req body

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,38 +1,57 @@
 import { Request, Response, NextFunction } from 'express';
+import { z } from 'zod';
 import { LoginType } from '../services/auth.service';
 import { AuthServices } from '../services/auth.service';
 
 const authServices = new AuthServices();
+const loginSchema = z.object({
+	username: z.string().min(1, 'Username required'),
+	password: z.string().min(1, 'Password required'),
+});
+
+const tokenSchema = z.object({
+	token: z.string().min(1, 'Token required'),
+});
+
 export class AuthController {
 	static async login(req: Request, res: Response, next: NextFunction) {
 		try {
-			const { username, password }: LoginType = req.body;
-			const token = await authServices.login({ username: username, password: password });
-      res.json(token);
+			const validate = loginSchema.safeParse(req.body);
+			if (!validate.success) {
+				res.status(400).json({ message: validate.error });
+			}
+			const token = await authServices.login(validate.data!);
+			res.json(token);
 		} catch (e) {
-      next(e);
-    }
+			next(e);
+		}
 	}
 
-  static async logout(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { token } = req.body;
-      const out = await authServices.logout(token);
-      
-      res.json(out);
-    } catch (e) {
-      next(e);
-    }
-  }
+	static async logout(req: Request, res: Response, next: NextFunction) {
+		try {
+			const validate = tokenSchema.safeParse(req.body);
+			if (!validate.success) {
+				res.status(400).json({ message: validate.error });
+			}
+			const out = await authServices.logout(validate.data?.token!);
 
-  static async refreshToken(req: Request, res: Response, next: NextFunction){
-    try {
-      const { token } = req.body;
-      const { accessToken } = await authServices.refresh(token);
+			res.json(out);
+		} catch (e) {
+			next(e);
+		}
+	}
 
-      res.json({ accessToken });
-    } catch (e) {
-      next(e);
-    }
-  }
+	static async refreshToken(req: Request, res: Response, next: NextFunction) {
+		try {
+			const validate = tokenSchema.safeParse(req.body);
+			if (!validate.success) {
+				res.status(400).json({ message: validate.error });
+			}
+			const { accessToken } = await authServices.refresh(validate.data?.token!);
+
+			res.json({ accessToken });
+		} catch (e) {
+			next(e);
+		}
+	}
 }
