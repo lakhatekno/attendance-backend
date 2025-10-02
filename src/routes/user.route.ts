2c6124d1c6d43ca77d42b6d8e3a19bef612d3dc1
Muhammad Islakha <lakhatekno2514@gmail.com>
2025-10-03 04:20:06 +0700

    [User] Implement Auth middleware

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user.controller';
+import { authenticate } from '../middlewares/auth.middleware';
+import { authorize } from '../middlewares/role.middleware';
 
 const router = Router();
 
-router.get('/', UserController.getUsers);
+router.get('/', authenticate, authorize('admin'), UserController.getUsers);
 router.post('/', UserController.createUser);
 router.post('/create-users', UserController.createUsers);
 
