129c785d34c5574b281bffffb76f92204981aa49
Muhammad Islakha <lakhatekno2514@gmail.com>
2025-10-02 19:10:18 +0700

    [Auth] Create auth routers with auths middleware

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { authenticate } from "../middlewares/auth.middleware";
+import { authorize } from "../middlewares/role.middleware";
+import { AuthController } from "../controllers/auth.controller";
 
 const router = Router();
+
+router.post('/login', AuthController.login);
+router.get('/logout', authenticate, authorize("admin", "user"), AuthController.logout);
 
 export default router;
