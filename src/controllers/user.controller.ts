aee7e882ce599d7821b74978f56290f33800f98d
lakhatekno <lakhatekno2514@gmail.com>
2025-10-02 19:41:29 +0700

    [User] Implement zod validation to user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,40 +1,52 @@
-import { Request, Response, NextFunction } from "express";
-import { UserServices } from "../services/user.service";
+import { Request, Response, NextFunction } from 'express';
+import { z } from 'zod';
+import { UserServices } from '../services/user.service';
 
 const userService = new UserServices();
 
+const userSchema = z.object({
+	email: z.email(),
+	username: z.string().min(1, 'Username required'),
+	password: z.string().min(8, 'Password minimum 8 character'),
+	name: z.string().min(1, 'Name required'),
+	role: z.enum(['admin', 'user']),
+});
+
 export class UserController {
-  static async getUsers(_req: Request, res: Response, next: NextFunction) {
-    try {
-      const users = await userService.getAllUsers();
-      res.json(users);
-    } catch(e) {
-      next(e);
-    }
-  }
+	static async getUsers(_req: Request, res: Response, next: NextFunction) {
+		try {
+			const users = await userService.getAllUsers();
+			res.json(users);
+		} catch (e) {
+			next(e);
+		}
+	}
 
-  static async createUser(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { email, username, password, name, role } = req.body;
-      const user = await userService.createUser({
-          email: email,
-          username: username,
-          password: password,
-          name: name,
-          role: role
-      });
-      res.status(201).json(user.id);
-    } catch(e) {
-      next(e);
-    }
-  }
+	static async createUser(req: Request, res: Response, next: NextFunction) {
+		try {
+			const validate = userSchema.safeParse(req.body);
+			if (!validate.success) {
+				res.status(400).json({ message: validate.error.issues[0] });
+			}
+			const user = await userService.createUser({
+				email: validate.data?.email!,
+				username: validate.data?.username!,
+				password: validate.data?.password!,
+				name: validate.data?.name!,
+				role: validate.data?.role!,
+			});
+			res.status(201).json(user.id);
+		} catch (e) {
+			next(e);
+		}
+	}
 
-  static async createUsers(req: Request, res: Response, next: NextFunction) {
-    try {
-      const users = await userService.createUsers(req.body);
-      res.status(201).json(users);
-    } catch(e) {
-      next(e);
-    }
-  }
+	static async createUsers(req: Request, res: Response, next: NextFunction) {
+		try {
+			const users = await userService.createUsers(req.body);
+			res.status(201).json(users);
+		} catch (e) {
+			next(e);
+		}
+	}
 }
